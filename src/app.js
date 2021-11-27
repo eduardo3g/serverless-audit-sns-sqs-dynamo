fbaaba2ff4cd1a49300d2447bf31d08de7b6794c
eduardo3g <eduardosbrasil10@gmail.com>
2021-11-27 17:28:07 -0300

    feat: publish deleted account eventos to SNS topic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-const deleteAccount = async event => {
-  console.log(event)
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+
+const deleteAccount = async (event) => {
+  const sns = new AWS.SNS()
+
+  const deleteAccountEvent = {
+    ...event,
+    timestamp: Math.floor(new Date().getTime() / 1000),
+  }
+
+  console.log(deleteAccountEvent)
+
+  return sns.publish({
+    Message: JSON.stringify(deleteAccountEvent),
+    TopicArn: process.env.SNS_TOPIC,
+  }).promise()
 }
 
 module.exports = {
