1e679f917a0d7d28499aff748b0f8f7403f474a1
eduardo3g <eduardosbrasil10@gmail.com>
2021-11-27 19:53:26 -0300

    feat: consume messages from SQS queue and store audit logs on DynamoDB audit table

--- src/consumer.js
+++ src/consumer.js
@@ -1,5 +1,21 @@
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const getUUID = require('uuid-by-string')
+
+const DocumentClient = new AWS.DynamoDB.DocumentClient()
+
 const recordDeletedAccountInfo = event => {
-  console.log(event)
+  const { email, timestamp } = JSON.parse(event.Records[0].body)
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      userId: getUUID(email, 'audit'),
+      deletedAt: timestamp,
+    },
+  }
+
+  return DocumentClient.put(params).promise()
 }
 
 module.exports = {
